b1afe5a99183aa5909e6960d1ca020fb8565e11a
Alberto Octavio Gonzalez Ruelas <alberto_glez8@MacBook-Pro-de-Alberto-Octavio-Gonzalez-Ruelas-2.local>
2017-09-08 16:02:00 -0500

    For and While Cycles

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+
+for (let i=0; i<5; i++) {
+    console.log("Hello, several times: "+ i);
+}
+for(let i=1; i<=4; i++) {
+    let line: string= "*";
+    line= line.repeat (i);
+    console.log(line);
+}
+
+let a =1;
+while(a<5) {
+    let line: string="*";
+    line=line.repeat(a);
+    console.log(line);
+    a++;
+}
